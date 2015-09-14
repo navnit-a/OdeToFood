98f8e15f43e53a4209fde267c2b4bbe6e4f0dcae
Navnit Anuth <nanuth@qaworks.com>
2015-09-14 22:47:55 +0100

    Initial Commit

---
+++ OdeToFood.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+
+namespace OdeToFood.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your app description page.";
+            //ViewBag.Location = "Watford, UK";
+
+            var model = new AboutModels
+            {
+                Name = "nAvz MVC",
+                Location = "Watford UK Man"
+            };
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
